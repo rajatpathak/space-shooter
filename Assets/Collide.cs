0a0ff7d9a84d09de0934ac7dc710351ceecd0d38
love <love@loves-MacBook-Pro.local>
2019-05-07 14:37:17 +0530

    first commit

---
+++ Assets/Collide.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collide : MonoBehaviour {
+
+	public int lives;
+	public GameObject[] liveGraph;
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.transform.tag == "Asteroid"){
+			if (lives > 0) {
+				lives--;
+				Debug.Log (lives);
+			}
+		}
+	}
+
+	void Update(){
+		if(lives == 3){
+			liveGraph [0].GetComponent<SpriteRenderer> ().enabled = true;
+			liveGraph [1].GetComponent<SpriteRenderer> ().enabled = true;
+			liveGraph [2].GetComponent<SpriteRenderer> ().enabled = true;
+		}
+		else if(lives == 2){
+			liveGraph [0].GetComponent<SpriteRenderer> ().enabled = true;
+			liveGraph [1].GetComponent<SpriteRenderer> ().enabled = true;
+			liveGraph [2].GetComponent<SpriteRenderer> ().enabled = false;
+		}
+		else if(lives == 1){
+			liveGraph [0].GetComponent<SpriteRenderer> ().enabled = true;
+			liveGraph [1].GetComponent<SpriteRenderer> ().enabled = false;
+			liveGraph [2].GetComponent<SpriteRenderer> ().enabled = false;
+		}
+		else if(lives == 0){
+			liveGraph [0].GetComponent<SpriteRenderer> ().enabled = false;
+			liveGraph [1].GetComponent<SpriteRenderer> ().enabled = false;
+			liveGraph [2].GetComponent<SpriteRenderer> ().enabled = false;
+		}
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# space-shooter
+
+![space-shooter-ikn](https://cloud.githubusercontent.com/assets/5364674/23105400/d3bb87d0-f6de-11e6-83c8-c0ea952a3efa.png)
+
+English
+-------
+Simple space shooter game from [Unity](https://unity3d.com/) workshops.
+
+[Graphics](http://kenney.nl/assets/space-shooter-extension) from [kenney.nl](http://kenney.nl/) on [CC0 1.0 Universal](https://creativecommons.org/publicdomain/zero/1.0/) license.
+
+
+Polski
+------
+Prosta kosmiczna strzelanka z warsztatów [Unity](https://unity3d.com/).
+
+[Grafiki](http://kenney.nl/assets/space-shooter-extension) pochodzą z [kenney.nl](http://kenney.nl/) i zostały udostępnione na licencji [CC0 1.0 Universal](https://creativecommons.org/publicdomain/zero/1.0/deed.pl).
